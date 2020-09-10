ac78596d206fddd26afbb4cdb683da6deb8d70fb
“kui <haobaikui163@163.com>
2020-09-10 14:47:04 +0800

    基本的框架

---
+++ src/main/java/com/example/mechanical/MechanicalApplication.java
@@ -0,0 +1,13 @@
+package com.example.mechanical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MechanicalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/mechanical/entity/User.java
@@ -0,0 +1,8 @@
+package com.example.mechanical.entity;
+
+/**
+ * @author HaoBaiKui
+ * @date 2020/9/10 11:15
+ */
+public class User {
+}
